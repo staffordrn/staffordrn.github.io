16efdbff7d87444b1eedf4d5edc524e5d16f9b32
staffordrn <sta18036@byui.edu>
2019-03-10 21:18:16 -0400

    laeeon 9 assignment

---
+++ CIT230/assignments/lesson-9/scripts/hamburger.js
@@ -0,0 +1,3 @@
+function togglemenu() {
+  document.getElementById("primarynav").classList.toggle("hide");
+}
---
+++ CIT230/assignments/lesson-9/scripts/towninfo.js
@@ -0,0 +1,50 @@
+let section = document.querySelector('section.towninfo');let requestURL = "https://byui-cit230.github.io/weather/data/towndata.json";
+//send request to retrieve JSON and store in a variable called townInfo
+let request = new XMLHttpRequest();
+request.open("GET", requestURL);
+request.responseType = "json";
+request.send();
+request.onload = function() {
+  var townInfo = request.response;
+  fillTownInfo(townInfo);
+}
+//function for creating and displaying town info
+  function fillTownInfo(jsonObj) {
+    let towns = jsonObj["towns"];
+    let townArray = ["Preston", "Fish Haven", "Soda Springs"];
+     
+    for (i=0; i < towns.length; i++) 
+      for (t=0; t < townArray.length; t++)
+      {
+        if (towns[i].name == townArray[t] ){
+        let box = document.createElement("article");
+        let boxh1 = document.createElement("h1");
+        let line1 = document.createElement("p");
+        let line2 = document.createElement("p");
+        let line3 = document.createElement("p");
+        let line4 = document.createElement("p");
+        let image = document.createElement("img");
+        let imgArray = ["foliage-256.jpg", "blue-sky-256.jpg", "autumn-256.jpg"];
+        
+        boxh1.textContent = towns[i].name;
+        line1.textContent = "Motto: " + towns[i].motto;
+        line2.textContent = "Year Founded: " + towns[i].yearFounded;
+        line3.textContent = "Population: " + towns[i].currentPopulation;
+        line4.textContent = "Annual Rainfall " + towns[i].averageRainfall + "in";  
+                
+        box.appendChild(boxh1);
+        box.appendChild(line1);
+        box.appendChild(line2);
+        box.appendChild(line3);
+        box.appendChild(line4);
+        for (p=0; p<imgArray.length;) {
+          image.setAttribute("src", "images/" +p+ ".jpg" );
+          box.appendChild(image);
+          p++;
+          break;
+        }
+
+        section.appendChild(box);
+      }
+    }  
+  }
