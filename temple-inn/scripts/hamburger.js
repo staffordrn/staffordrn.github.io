be80c702af4f765a275df22a45a086d2c1b888a4
staffordrn <sta18036@byui.edu>
2019-04-12 09:46:36 -0400

    temole inn commit 2

--- temple-inn/scripts/hamburger.js
+++ temple-inn/scripts/hamburger.js
@@ -1,18 +1,1 @@
-function toggleMenu() {
-  document.getElementById("nav").classList.toggle("hide");
-}
-function toggleSubMenu() {
-  document.getElementById("subnav").classList.toggle("hide");
-}
-function toggleMoreInfo0() {
-  document.getElementById("moreinfo0").classList.toggle("hide");
-}
-function toggleMoreInfo1() {
-  document.getElementById("moreinfo1").classList.toggle("hide");
-}
-function toggleMoreInfo2() {
-  document.getElementById("moreinfo2").classList.toggle("hide");
-}
-function toggleMoreInfo3() {
-  document.getElementById("moreinfo3").classList.toggle("hide");
-}
+function toggleMenu(){document.getElementById("nav").classList.toggle("hide")}function toggleSubMenu(){document.getElementById("subnav").classList.toggle("hide")}function toggleMoreInfo0(){document.getElementById("moreinfo0").classList.toggle("hide")}function toggleMoreInfo1(){document.getElementById("moreinfo1").classList.toggle("hide")}function toggleMoreInfo2(){document.getElementById("moreinfo2").classList.toggle("hide")}function toggleMoreInfo3(){document.getElementById("moreinfo3").classList.toggle("hide")}
